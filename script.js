b367c870bfc59129cce981ce8b5012311b93a47f
SirioRedGiant <Oiris1992@gmail.com>
2025-12-17 17:51:21 +0100

    aggiunta formattazione log con il css

--- script.js
+++ script.js
@@ -58,25 +58,43 @@ let FizzMultiplier = 3;
 const secondReplacerName = "Buzz";
 let BuzzMultiplier = 5;
 
-//! formattazione dei log
+//! definizione degli stili per ogni output
+const styleFizz =
+  "background: #f79e19ff; color: black; padding: 2px 5px; font-weight: bold;";
+const styleBuzz =
+  "background: #46b5ffff; color: black; padding: 2px 5px; font-weight: bold;";
+const styleFizzBuzz =
+  "background: #b9f247ff; color: black; padding: 2px 5px; font-weight: bold;";
 
 for (let i = 0; i <= 100; i++) {
   // dichiaro una variabile vuota:
   let BosseswishedOutput = "";
+
   // se il numero è multiplo di 3:
   if (i % FizzMultiplier === 0) {
     BosseswishedOutput += firstReplacerName;
   }
+
   // se il numero è multiplo di 5:
   if (i % BuzzMultiplier === 0) {
     BosseswishedOutput += secondReplacerName;
   }
 
-  //  oppure questa => console.log(BosseswishedOutput || i);
-  if (BosseswishedOutput !== '') {
-    console.log(BosseswishedOutput);
-  }
-   else {
+  if (BosseswishedOutput !== "") {
+    //! -> inizio di formattazione CSS <-
+    let currentStyle = "";
+    if (BosseswishedOutput === "Fizz") {
+      currentStyle = styleFizz;
+    } else if (BosseswishedOutput === "Buzz") {
+      currentStyle = styleBuzz;
+    } else if (BosseswishedOutput === "FizzBuzz") {
+      currentStyle = styleFizzBuzz;
+    }
+    //! aggiungo segnaposto per applicare lo stile CSS definito sopra
+    console.log(`%c${BosseswishedOutput}`, currentStyle);
+  } else {
     console.log(i);
   }
 }
+
+//  => console.log(BosseswishedOutput || i); oppure queste condizioni sotto
